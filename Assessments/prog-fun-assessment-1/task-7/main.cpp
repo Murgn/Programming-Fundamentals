4200678453eb76df7c6cd1b6cad0e6bc7fe2676b
Morgan <murgntheurgn@gmail.com>
2024-10-28 22:24:33 +0000

    Update main.cpp

--- Assessments/prog-fun-assessment-1/task-7/main.cpp
+++ Assessments/prog-fun-assessment-1/task-7/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
 using namespace std;
 
 // In this task, I learnt how to use enums in C++, I am used to C# so it took a while to learn
@@ -38,6 +39,9 @@ struct Player
     string name = "";
     Weapon weapon;
 };
+
+string RolesToString(int i);
+void ClearInput();
 
 const unsigned int playerCount = 3;
 Player players[playerCount];
@@ -81,7 +85,9 @@ int main(int argc, char* argv[])
             i++;
         }
 
-        for (Player player : players)
+        // Took me a while to bug fix this. I learn that the array im editing in a for each loop is a copy.
+        // If I want to edit the original array I have to pass it in as a reference using the & operator.
+        for (Player &player : players)
         {
             while (true)
             {
@@ -92,22 +98,104 @@ int main(int argc, char* argv[])
                 // Error Handling
                 if (cin.fail())
                 {
-                    cin.clear();
-                    cin.ignore(1000, '\n');
+                    ClearInput();
 
                     cerr << "[!] Invalid input, try again." << endl;
                     continue;
                 }
+                int modifiedInput = input - 1;
 
-                if ((input - 1 < sizeof(weapons) / sizeof(Weapon) && input > -1))
+                if (!(modifiedInput < sizeof(weapons) / sizeof(Weapon) && modifiedInput > -1))
                 {
                     cerr << "[!] Invalid input, please choose a valid item to purchase, try again." << endl;
                     continue;
                 }
 
+
+                if (!weapons[modifiedInput].bought)
+                {
+                    if (coins >= weapons[modifiedInput].price)
+                    {
+                        weapons[modifiedInput].bought = true;
+                        player.weapon = weapons[modifiedInput];
+                        coins -= weapons[modifiedInput].price;
+                        cout << player.name << " bought a " << weapons[modifiedInput].name << " for " << weapons[modifiedInput].price << " coins. The party now has " << coins << " coins." << endl;
+                        break;
+                    }
+                    else
+                    {
+                        cerr << "[!] Invalid input, not enough coins, the " << weapons[modifiedInput].name << " is " << weapons[modifiedInput].price << " coins and the party only has " << coins << " coins. try again." << endl;
+                        continue;
+                    }
+                }
+                else 
+                {
+                    cerr << "[!] Invalid input, this weapon has already been bought, try again." << endl;
+                    continue;
+                }
+
             }
         }
+
+        string confirmation = "";
+        bool restart = false;
+        ClearInput();
+        while (true)
+        {
+            cout << "Is your party happy with your weapons? (yes/no): ";
+            getline(cin, confirmation);
+            if (confirmation == "yes") { break; }
+            // I'm using a boolean and doing it this way because I'm not sure how to break out of this#
+            // nested while loop and then continue the main while loop any other way.
+            else if (confirmation == "no") { restart = true; break; }
+            else 
+            {
+                cerr << "[!] Invalid input, unrecognised command, try again." << endl;
+                continue;
+            }
+        }
+
+        if (restart) continue;
+        else break;
+    }
+
+    cout << "Your Party: " << endl;
+    for (Player player : players)
+    {
+        cout << "\t" << player.name << " the " << RolesToString(player.weapon.role) << ". [Weapon: " << player.weapon.name << ", Damage: " << player.weapon.damage << "]" << endl;
+    }
+
+    string encounter = "You delve deeper into the forbidden forest. A monster appears...";
+
+    for (char c : encounter) 
+    {
+        cout << c;
+        Sleep(100);
     }
 
     return 0;
+}
+
+// Made a function to convert the roles enum numbers to strings.
+string RolesToString(int i)
+{
+    switch (i)
+    {
+    default: return "No Role";
+    case 1:
+        return "Knight";
+    case 2:
+        return "Archer";
+    case 3:
+        return "Spearman";
+    }
+}
+
+// I was having some issues with my party weapon confirmation auto-inputting and telling the
+// player that there was an unrecognised command, so I made this function to clear the input
+// buffer before we ask for a command.
+void ClearInput()
+{
+    cin.clear();
+    cin.ignore(1000, '\n');
 }
